dea004c86af582cc3fe891b324c4007bdb55eab9
sehong3 <a93245124@gmail.com>
2023-09-04 18:02:36 +0900

    add edittest.py

---
+++ edittest.py
@@ -0,0 +1,15 @@
+a = "hello"
+b = "world"
+print(a, b)
+print(b)
+print(b, a)
+print("python", a, b)
+number_school = 2394584903
+str_name = "python"
+bool_view = False
+flt_pi = 3.14
+char_element = 'a'
+chars = "hello world"
+print("number : ", number_school, " name : ", str_name, "bool : ", bool_view, " float : ", flt_pi, " charcter : " , char_element, " charcters : ", chars)
+
+#git push
